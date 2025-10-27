97f8f8c644252543f0c0f7f60634345e78ea23bb
Love Kumar <lovekumaramrani13@gmail.com>
2025-10-27 04:28:55 -0700

    page completed

--- src/app/ffc-volunteer-proving-ground-core-competencies/page.jsx
+++ src/app/ffc-volunteer-proving-ground-core-competencies/page.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Header from "@/components/ffc-volunteer-proving-ground-core-competencies/Header";
+import ContentSection from "@/components/ffc-volunteer-proving-ground-core-competencies/ContentSection";
+import Modulessection from "@/components/ffc-volunteer-proving-ground-core-competencies/Modules-section";
+import Footer from "@/components/ffc-volunteer-proving-ground-core-competencies/Footer";
 
 const index = () => {
-  return <div className="flex items-center justify-center min-h-screen text-[34px] w-full text-center">
-    <h1 className="text-center">ffc-volunteer-proving-ground-core-competencies</h1>
-  </div>;
+  return (
+    <div className="w-full pt-[150px]" id="segoe-font">
+      <div className="w-full max-w-[90%] md:max-w-[80%] md:p-[2rem] mx-auto">
+        <Header />
+        <ContentSection />
+        <Modulessection />
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default index;
