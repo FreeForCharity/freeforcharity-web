588ab7e5334484b2e9544e2bf1c7d803f7b03481
Love Kumar <lovekumaramrani13@gmail.com>
2025-10-28 04:22:35 -0700

    updated

--- src/components/free-for-charity-endowment-fund-components/Empower-Charities/index.tsx
+++ src/components/free-for-charity-endowment-fund-components/Empower-Charities/index.tsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { CSSProperties, FC, IframeHTMLAttributes } from "react";
 
-const index = () => {
-  return (
-    <div>
-      Empower-Charities
-    </div>
-  )
+interface ExtendedIframeProps extends IframeHTMLAttributes<HTMLIFrameElement> {
+  allowpaymentrequest?: string;
 }
 
-export default index
+const DonationSection: FC = () => {
+  const commonStyle: CSSProperties = {
+    position: "absolute",
+    border: "0",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    width: "100%",
+  };
+
+  const donationFormStyle: CSSProperties = {
+    ...commonStyle,
+    height: "100%",
+  };
+
+  const thermometerStyle: CSSProperties = {
+    ...commonStyle,
+    height: "120px",
+  };
+
+  const donationFormProps: ExtendedIframeProps = {
+    title: "Donation form powered by Zeffy",
+    style: donationFormStyle,
+    src: "https://www.zeffy.com/embed/donation-form/free-for-charity-endowment-fund",
+    allowpaymentrequest: "",
+    allowTransparency: true,
+  };
+
+  const thermometerProps: ExtendedIframeProps = {
+    title: "Donation thermometer powered by Zeffy",
+    style: thermometerStyle,
+    src: "https://www.zeffy.com/embed/thermometer/free-for-charity-endowment-fund",
+    allowTransparency: true,
+  };
+
+  return (
+    <div className="bg-[#003566] py-[54px]">
+      <div className="w-[90%] md:w-[80%] mx-auto  py-[27px]">
+        <div className="mb-[10px]">
+          <h3
+            className="text-[32px] font-[500] leading-[42px] pb-[10px] text-white"
+            id="cinzel"
+          >
+            Empower Charities with Your Generosity
+          </h3>
+        </div>
+
+        <div className="h-full min-h-[1374px] pt-[120px] ">
+          {/* Thermometer Embed */}
+          <div className="relative w-full h-[120px]">
+            <iframe {...thermometerProps}></iframe>
+          </div>
+        </div>
+
+        <div className="h-full min-h-[1200px] bg-white space-y-10">
+          {/* Donation Form */}
+          <div className="relative w-full h-[1200px]">
+            <iframe {...donationFormProps}></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DonationSection;
