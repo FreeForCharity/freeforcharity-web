8d51cf24b0d7418ab8d80be9c9e41d6095cbff5f
Love Kumar <lovekumaramrani13@gmail.com>
2025-11-02 03:28:14 -0800

    updated

--- src/app/Figma-Home-page/index.tsx
+++ src/app/Figma-Home-page/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "@/components/Figma-Home-Page-Components/Hero";
+import Mission from "@/components/Figma-Home-Page-Components/Mission";
 
 const index = () => {
   return (
     <div>
       <Hero />
+      <Mission />
     </div>
   );
 };
---
+++ src/components/Figma-Home-Page-Components/Mission/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div className="py-[52px]">
+      <div className="w-[90%] mx-auto py-[27px] mb-[60px]">
+        <h1
+          className="font-[400] text-[40px] lg:text-[48px] leading-[100%] tracking-[0] text-center w-full md:w-[906px] mx-auto mb-[50px]"
+          id="faustina-font"
+        >
+          Free For Charity has a simple mission with broad implications
+        </h1>
+        <p
+          className="font-[700] text-[25px] leading-[150%] tracking-[0] text-center mb-[30px]"
+          id="lato-font"
+        >
+          Reduce costs and increase revenues for nonprofits; putting that money
+          back into their charitable mission where it belongs.
+        </p>
+        <p
+          className="font-[500] text-[25px] leading-[150%] tracking-[0] text-center"
+          id="lato-font"
+        >
+          This charity for charities seeks to replace as many functions as
+          possible that current nonprofits pay for to for-profit companies with
+          free or at cost work from our campus, on site projects, or
+          partnerships with other entities.
+        </p>
+      </div>
+
+      <div className="w-[95%] mx-auto bg-[#2B627B] h-[1px]"></div>
+    </div>
+  );
+};
+
+export default index;
