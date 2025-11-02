ff69cb2f8107d86b37dbed78fd24179065d755f4
Love Kumar <lovekumaramrani13@gmail.com>
2025-11-02 23:18:50 +0500

    updatedd

--- src/app/Figma-Home-page/index.tsx
+++ src/app/Figma-Home-page/index.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Figma-Home-Page-Components/Hero";
 import Mission from "@/components/Figma-Home-Page-Components/Mission";
 import SupportFreeForCharity from "@/components/Figma-Home-Page-Components/SupportFreeForCharity";
 import EndowmentFeatures from "@/components/Figma-Home-Page-Components/Endowment-Features";
+import OurPrograms from "@/components/Figma-Home-Page-Components/Our-Programs";
 
 const index = () => {
   return (
@@ -11,6 +12,7 @@ const index = () => {
       <Mission />
       <SupportFreeForCharity />
       <EndowmentFeatures />
+      <OurPrograms />
     </div>
   );
 };
---
+++ src/components/Figma-Home-Page-Components/Our-Programs/index.tsx
@@ -0,0 +1,182 @@
+import React from "react";
+import Image from "next/image";
+import OrangeFaqItem from "@/components/UI/OrangeFaqItem";
+
+const index = () => {
+  return (
+    <div className="py-[52px]">
+      <div className="w-[90%] lg:px-[20px] mx-auto">
+        <h1
+          className="font-[400] text-[40px] lg:text-[48px]  tracking-[0] text-center mx-auto mb-[50px]"
+          id="faustina-font"
+        >
+          Our Programs
+        </h1>
+
+        <div className="lg:pl-[50px]">
+          <div className="mb-[40px]  flex items-center gap-[20px]">
+            <div className="w-[100px] flex items-center justify-center p-2 h-[100px] bg-[#2A6682] rounded-full">
+              <div className="relative w-[56px] h-[56px]">
+                <Image
+                  src="/Svgs/FFC-Domains.svg"
+                  alt="FFC-Domains"
+                  fill
+                ></Image>
+              </div>
+            </div>
+            <h1 className="text-[36px] font-[400] " id="lato-font">
+              FFC Domains
+            </h1>
+          </div>
+          <p className="text-[25px] font-[400] " id="lato-font">
+            Provides free .org domain names, Microsoft 365 with Outlook email, &
+            Microsoft Teams to 501c3 charities.
+          </p>
+          <div className="flex items-center justify-center py-[30px]">
+            <a
+              href="#"
+              className="inline-flex items-center justify-center w-[328px] h-[62px] rounded-[27px] gap-[10px] 
+             px-[32px] py-[18px] bg-[#2A6682] text-white opacity-100 text-[25px]  font-[400]"
+              id="lato-font"
+            >
+              Learn More and Apply
+            </a>
+          </div>
+        </div>
+
+        {/* faqs  */}
+        <div>
+          <OrangeFaqItem title=".org Domain Registration">
+            <ul className="list-disc list-inside">
+              <li className="">
+                For You: Leverage a .org domain name to enhance your charity's
+                credibility, trustworthiness, and online presence, making it
+                easier to attract donors and supporters
+              </li>
+            </ul>
+          </OrangeFaqItem>
+          <OrangeFaqItem title="Cloudflare DNS">
+            <ul className="list-disc list-inside">
+              <li className="">
+                For You: Faster website load times and enhanced security
+              </li>
+              <li className="">
+                For Us: Centralized management and automation tools.
+              </li>
+            </ul>
+          </OrangeFaqItem>
+          <OrangeFaqItem title="Charity Email Address">
+            <ul className="list-disc list-inside">
+              <li className="">
+                For You: Using a charity email address (e.g.,
+                yourname@yourcharity.org) enhances your organization's
+                credibility and professionalism, making it easier to build trust
+                with donors, volunteers, and stakeholders
+              </li>
+              <li className="">
+                For Us: We benefit by ensuring charities use professional email
+                addresses, which helps maintain our servers' integrity and
+                provides a more secure and reliable communication platform for
+                our volunteers
+              </li>
+            </ul>
+          </OrangeFaqItem>
+          <OrangeFaqItem title="Microsoft 365">
+            <ul className="list-disc list-inside">
+              <li className="">
+                For You: Professional email and collaboration tools
+              </li>
+              <li className="">
+                For Us: Streamlined communication and support processes
+              </li>
+            </ul>
+          </OrangeFaqItem>
+        </div>
+
+        <div className="mt-[60px]">
+          <div className="lg:pl-[50px] mb-[40px]  flex items-center gap-[20px]">
+            <div className="w-[100px] flex items-center justify-center p-2 h-[100px] bg-[#2A6682] rounded-full">
+              <div className="relative w-[56px] h-[56px]">
+                <Image
+                  src="/Svgs/FFC-Hosting.svg"
+                  alt="FFC-Domains"
+                  fill
+                ></Image>
+              </div>
+            </div>
+            <h1 className="text-[36px] font-[400]  " id="lato-font">
+              FFC Hosting
+            </h1>
+          </div>
+          <p className="text-[25px] font-[400]  " id="lato-font">
+            Free shared hosting for nonprofit organizations, along with
+            WordPress installation and management, and access to our premium
+            plugins and themes from:
+          </p>
+          <div className="flex items-center justify-center py-[30px]">
+            <a
+              href="#"
+              className="inline-flex items-center justify-center w-[328px] h-[62px] rounded-[27px] gap-[10px] 
+             px-[32px] py-[18px] bg-[#2A6682] text-white opacity-100 text-[25px]  font-[400]"
+              id="lato-font"
+            >
+              Learn More and Apply
+            </a>
+          </div>
+        </div>
+
+        {/* faqs  */}
+        <div>
+          <OrangeFaqItem title="Northwest Registered Agent">
+            <ul className="list-disc list-inside">
+              <li className="">
+                For You: Leverage Northwest Registered Agent's services to
+                maintain compliance with state requirements, including
+                registered agent services, nonprofit corporation filing, and
+                initial charity IRS application.
+              </li>
+              <li className="">
+                For Us: Efficiently support charities by ensuring they meet
+                legal requirements and train our volunteers on managing
+                compliance, business formation processes, and IRS applications.
+              </li>
+            </ul>
+          </OrangeFaqItem>
+          <OrangeFaqItem title="VolunteerMatch">
+            <ul className="list-disc list-inside">
+              <li className="">
+                For You: Access to a large pool of potential volunteers
+              </li>
+              <li className="">
+                For Us: Validation of your active community engagement
+              </li>
+            </ul>
+          </OrangeFaqItem>
+          <OrangeFaqItem title="TechSoup">
+            <ul className="list-disc list-inside">
+              <li className="">
+                For You: Access to discounted software and technology resources
+              </li>
+              <li className="">
+                For Us: Additional validation of your non-profit status
+              </li>
+            </ul>
+          </OrangeFaqItem>
+          <OrangeFaqItem title="PayPal">
+            <ul className="list-disc list-inside">
+              <li className="">
+                For You: Easy, secure online donation processing
+              </li>
+              <li className="">
+                For Us: Standardized financial transaction system for all
+                partners
+              </li>
+            </ul>
+          </OrangeFaqItem>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
