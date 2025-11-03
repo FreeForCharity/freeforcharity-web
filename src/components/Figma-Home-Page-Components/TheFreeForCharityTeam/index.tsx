b7fde7c1d1ae8cb0626a206400447ac7c023db21
Love Kumar <lovekumaramrani13@gmail.com>
2025-11-03 10:08:29 +0500

    updated

--- src/components/Figma-Home-Page-Components/TheFreeForCharityTeam/index.tsx
+++ src/components/Figma-Home-Page-Components/TheFreeForCharityTeam/index.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import TeamMemberCard from "@/components/UI/TeamMemberCard";
 
 const index = () => {
-  return <div>The Free For Charity Team</div>;
+  return (
+    <div className="py-[50px]">
+      <div className="w-[90%] mx-auto py-[40px]">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  items-stretch justify-center mb-[50px] gap-[30px]">
+          <TeamMemberCard
+            imageUrl="/Images/member1.png"
+            name="Clarke Moyer"
+            title="Free For Charity Founder/ President of the Board"
+            linkedinUrl="https://www.linkedin.com/in/clarkemoyer/"
+          />
+          <TeamMemberCard
+            imageUrl="/Images/member2.png"
+            name="Chris Rae"
+            title="Free For Charity Vice President"
+            linkedinUrl="https://www.linkedin.com/in/clarkemoyer/"
+          />
+          <TeamMemberCard
+            imageUrl="/Images/member3.png"
+            name="Tyler Carlotto"
+            title="Free For Charity Secretary"
+            linkedinUrl="https://www.linkedin.com/in/clarkemoyer/"
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center mt-[40px] gap-[30px]">
+          <TeamMemberCard
+            imageUrl="/Images/member4.png"
+            name="Brennan Darling"
+            title="Free For Charity Treasurer"
+            linkedinUrl="https://www.linkedin.com/in/clarkemoyer/"
+          />
+          <TeamMemberCard
+            imageUrl="/Images/member5.png"
+            name="Rebecca Cook"
+            title="Free For Charity Member at Large"
+            linkedinUrl="https://www.linkedin.com/in/clarkemoyer/"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default index;
